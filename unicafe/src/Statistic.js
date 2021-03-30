3829e36b8ece0db79bf11e01d3bb12641ecd3ed8
Devin Don <i.inf@outlook.com>
2021-03-30 15:34:56 +0800

    1.7

--- unicafe/src/Statistic.js
+++ unicafe/src/Statistic.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Statistic = ({ good, neutral, bad }) => <>
-  <h2>Statistic</h2>
-  <p>Good: {good}</p>
-  <p>Neutral: {neutral}</p>
-  <p>Bad: {bad}</p>
-</>;
+const Statistic = ({ good, neutral, bad }) => {
+  const sum = good + neutral + bad;
+
+  return <>
+    <h2>Statistic</h2>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>All: {sum}</p>
+    <p>Avg: {(good - bad) / sum}</p>
+    <p>Positive: {good / sum}</p>
+  </>;
+};
 
 export default Statistic;
