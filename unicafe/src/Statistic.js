c113d87ae6ac2cde28963b177d61bcbaf4282597
Devin Don <i.inf@outlook.com>
2021-03-30 15:38:00 +0800

    1.9

--- unicafe/src/Statistic.js
+++ unicafe/src/Statistic.js
@@ -6,12 +6,20 @@ const Statistic = ({ good, neutral, bad }) => {
 
   return <>
     <h2>Statistic</h2>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-    <p>All: {sum}</p>
-    <p>Avg: {(good - bad) / sum}</p>
-    <p>Positive: {good / sum}</p>
+    {
+      sum > 0
+        ? <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {sum}</p>
+          <p>Avg: {(good - bad) / sum}</p>
+          <p>Positive: {good / sum}</p>
+        </>
+        : <>
+          <p>No feedback given.</p>
+        </>
+    }
   </>;
 
 };
