b9c744db1990d82d8cdd8b4f79b394b9a14be340
Devin Don <i.inf@outlook.com>
2021-03-30 17:46:55 +0800

    2.6

---
+++ phonebook/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={({ target: { value } }) => setNewName(value)} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(({ name }) => <p key={name}>{name}</p>)}
+    </div>
+  );
+
+};
+
+export default App;
